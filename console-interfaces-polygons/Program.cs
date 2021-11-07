07b6e0ccb59beadaf894935f6d3dbc351f0f8209
gavrun <38539655+gavrun@users.noreply.github.com>
2021-11-07 14:05:09 +0300

    Add project files.

---
+++ console-interfaces-polygons/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace console_interfaces_polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using concrete class
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+
+            //
+
+
+            //
+
+        }
+
+        //additional off topic
+        public static void DisplayPolygon(string polygonType, dynamic polygon)
+        {
+            //try-catch statement 
+            try
+            {
+                Console.WriteLine("{0} Number of Sides: {1}", polygonType, polygon.NumberOfSides);
+                Console.WriteLine("{0} Side Length: {1}", polygonType, polygon.SideLength);
+                Console.WriteLine("{0} Perimeter: {1}", polygonType, polygon.GetPerimeter());
+                Console.WriteLine("{0} Area: {1}", polygonType, Math.Round(polygon.GetArea(), 2));
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown while trying to process {0}:\n   {1}",
+                    polygonType, ex.GetType().Name);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ library-interfaces-polygons/ConcreteRegularPolygon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace library_interfaces_polygons
+{
+    public class ConcreteRegularPolygon
+    {
+        //automatic properties
+        public int NumberOfSides { get; set; }
+        //public int SideLength { get; set; }
+
+        //full property
+        private int sideLength;
+
+        public int SideLength
+        {
+            get { return sideLength; }
+            set { sideLength = value; }
+        }
+
+        //constructor
+        public ConcreteRegularPolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        //method for all polygones
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        //each polygon has its own method
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
