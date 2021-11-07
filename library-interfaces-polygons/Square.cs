db7d7f0b0cabfed0cad7b62e46b464b6396b66d3
gavrun <38539655+gavrun@users.noreply.github.com>
2021-11-07 14:09:48 +0300

    created concrete class

---
+++ library-interfaces-polygons/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_interfaces_polygons
+{
+    class Square : ConcreteRegularPolygon
+    {
+        //base constructor
+        public Square(int length) : base(4, length)
+        { }
+
+        //each polygon has its own method
+        //not forced to provide child method here
+        public override double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+
+    }
+}
